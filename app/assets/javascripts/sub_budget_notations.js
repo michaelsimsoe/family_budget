d6ab8e96af24cbfb883548ca510b0f6a9aba2336
Michael Krøysert-Simsø <mikke.korgen@gmail.com>
2018-05-26 10:01:39 +0200

    Add fixeds to UI.

--- app/assets/javascripts/sub_budget_notations.js
+++ app/assets/javascripts/sub_budget_notations.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+	var selectGroup = "";
+	var infoMessage = "";
+	
+	$('.new-btn').on('click', function() {
+		this.parentNode.parentNode.parentNode.classList.add('current');
+	})
+
+	$('input#withdrawal').on('change', function() {
+		// $('#pbn-info-message')
+		selectGroup = $(this)[0].parentNode.nextElementSibling;
+		infoMessage = selectGroup.parentNode.parentNode.children[0];
+		infoMessage.classList.add("out-visible");
+		selectGroup.classList.remove('hidden');
+	})
+	$('input#deposit').on('change', function() {
+		selectGroup = $(this)[0].parentNode.nextElementSibling;
+		infoMessage = selectGroup.parentNode.parentNode.children[0];
+		infoMessage.classList.remove("out-visible");
+		selectGroup.classList.add('hidden');
+	})
 	$('.new-btn').on('click', function() {
 		this.parentNode.parentNode.parentNode.classList.add('current');
 	})
